8a412e07d540a70708ad4320af2e023d08acf741
Totybeca <emeraldtoyin@gmail.com>
2022-07-05 10:59:37 -0700

    Added 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int _putchar(char value)
+{
+putchar(value);
+putchar("\n");
+
+}
+
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+return (0);
+}
