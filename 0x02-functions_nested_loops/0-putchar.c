72e996d1fc8e5868658f672fcaad45b5e0244a42
Totybeca <emeraldtoyin@gmail.com>
2022-07-05 11:24:27 -0700

    Added 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,18 @@
 /**
 * main - Entry point
 *
-<<<<<<< HEAD
 * Return: always 0 (success)
 */
-int main(void)
-{
-int _putchar(char value)
+
+void _putchar(char value)
 {
 putchar(value);
 putchar("\n");
 
 }
-=======
-* Return: Always 0 (Success)
-*/
+
 int main(void)
 {
->>>>>>> 7908e01d482880696392ea54aeb36f9e3f295285
 
 _putchar('p');
 _putchar('u');
@@ -27,13 +22,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-<<<<<<< HEAD
-
-return (0);
-=======
-_putchar('\n');
 
 return (0);
 
->>>>>>> 7908e01d482880696392ea54aeb36f9e3f295285
 }
