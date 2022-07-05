f857995516873faac7df8c633e78582ce2e3d2d6
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-05 20:33:15 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0 (success)
+*/
+
+void print_alphabet(){
+for(char i = 'a'; i <= 'z'; i++){
+putchar(i);
+}
+}
+
+int main(void)
+{
+  
+print_alphabet();
+putchar('\n');
+return (0);
+}
