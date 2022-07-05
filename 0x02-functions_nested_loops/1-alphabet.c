e45f3a1952e5206ceb734d1fa6b9017077c25003
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-05 22:30:37 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
 * Return: always 0 (success)
-* 
 * print_alphabet function to print lowercase alphabet
-*
+*/
+
+/**
+* print_alphabet function to print lowercase alphabet
 */
 void print_alphabet(void)
 {
