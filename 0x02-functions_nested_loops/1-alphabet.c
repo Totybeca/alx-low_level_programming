2fb53cdd348cd9290f5d6958af6d78f87c64141f
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-05 21:49:20 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,18 +5,13 @@
 * Return: always 0 (success)
 */
 
-print_alphabet();
 
 int main(void)
 {
-    print_alphabet();
-    return (0);
-}
-
-void print_alphabet(){
-  char i = 'a';
+     char i = 'a';
   for(i = 'a'; i <= 'z'; i++){
       putchar(i);
   }
+    return (0);
 }
 
