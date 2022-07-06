64c21b11f232a8e8a4b70a8bacfeb672c8b72538
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-06 01:56:47 +0100

    Update 4-isalpha.c
    Added an is alpha file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,19 @@
 
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - function to check for lowercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+int alpha = 0;
+if (isalpha(c))
+{
+alpha = 1;
+}
+return (alpha);
+}
