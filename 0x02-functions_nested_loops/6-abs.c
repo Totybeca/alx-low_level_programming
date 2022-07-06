dbcad736e0c94b247bb048bbde3008b4f346fb4d
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-06 03:24:15 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _abs - function to check for absolute value of an integer.
+ * @c:  is the int that will use for the argument of the function
+ * Return abs(value)
+int _abs(int c)
+{
+int val = abs(c);
+return (val);
+}
