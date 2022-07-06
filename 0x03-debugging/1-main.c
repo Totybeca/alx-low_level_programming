08d190e9b896c7adfadcf37eeb04f113b66b783d
Totybeca <emeraldtoyin@gmail.com>
2022-07-06 10:45:03 -0700

    Added a 2 largest_number.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - function to check for infinite loop
+ * is the int that will use for the argument of the function
+ * Return: 0
+ */
 int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/**
-* while (i < 10)
-* {
-* putchar(i);
-* }
-*/
+/* while (i < 10)*/
+/* {*/
+/* putchar(i);*/
+/*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
