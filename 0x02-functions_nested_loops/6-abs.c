e8469761c569dbb9e12822becef7920ae17a7cd3
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-06 03:25:34 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,6 +6,7 @@
  * _abs - function to check for absolute value of an integer.
  * @c:  is the int that will use for the argument of the function
  * Return abs(value)
+ */
 int _abs(int c)
 {
 int val = abs(c);
