03509b5017a71eb5f5b84dfbd2ba9599704e101c
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-06 16:19:33 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,5 @@
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 void _putchar(char c);
@@ -15,4 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
- void jack_bauer(void);
+void jack_bauer(void);
