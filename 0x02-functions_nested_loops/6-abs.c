23f3d156cf5d490a607b2e7af48e3c4e36daad37
Fashemore Toyin <emeraldtoyin@gmail.com>
2022-07-06 03:28:12 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
-
+#include <stdlib.h>
 /**
  * _abs - function to check for absolute value of an integer.
  * @c:  is the int that will use for the argument of the function
