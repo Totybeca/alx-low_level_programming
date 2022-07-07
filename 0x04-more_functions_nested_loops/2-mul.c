d9673a290cacc8e5e57ff99983ab0057977bf9fa
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 06:18:36 -0700

    Added a 2-mul.c file for multiplication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - It is a function that checks if a character is uppercase
+* @a: This is the first input
+* @b: This is the second input
+* Return: multiplication of two integers
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
