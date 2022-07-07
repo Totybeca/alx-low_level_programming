556b1a053d53d2cd9b74340fefc8f6b927291763
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 14:38:51 -0700

    Added a 6-print_line.c file to mre functions and nested loops folder

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_line - It is a function that prints straight line
+* @n: input
+*/
+
+void print_line(int n)
+{
+
+if (n > 0)
+{
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
