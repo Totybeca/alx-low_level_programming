ade0939dbf98dfb3c4765bb4c05d48d3ab8636f1
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 06:41:23 -0700

    Added a new file called  3-print_numbers.c to 0x04 more functions and nested loop folder

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* printing_numbers - It is a function that prints numbers from 0 to 9.
+*
+* Return: It returns 0 to 9 after which a new line is made.
+*/
+
+void print_numbers(void);
+{
+int j;
+for (j = 0; j <= 9; jtt)
+_putchar("%d, j");
+_putchar("\n");
+}
