894566c5c941585d0cad44ee6bfbc2ee8f5738ac
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 11:56:47 -0700

    Added 5-more numbers file to 0x04 more functions and nested loops folder

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,12 @@
 #include "main.h"
 /**
 * more_numbers - function to print 0-14 10 times
-*Return: 0
+* Return: Numbers from 0-14 10 times
 */
 void more_numbers(void)
 {
-int  j;
-int  i;
+int i;
+int j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
