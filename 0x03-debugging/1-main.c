ba747022012a3feca65438197efabdd122d415a7
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 01:36:54 -0700

    Added a 1-main.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - function to check for infinite loop
  * is the int that will use for the argument of the function
  * Return: 0
  */
+
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 /* while (i < 10)*/
 /* {*/
 /* putchar(i);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
