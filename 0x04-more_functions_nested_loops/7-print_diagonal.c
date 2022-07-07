ab0c274b613d30530ee4bc17fbc4447d90dd754a
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 16:21:48 -0700

    Added a 7-print_diagonal file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,12 +3,14 @@
 
 /**
 * print_diagonal - function to print diagonal line
-* @n:  is the int that will use for the argument of the function
+* @n: is the int that will use for the argument of the function
 */
 
 void print_diagonal(int n)
 {
+
 int i;
+
 if (n > 0)
 {
 for (i = 0; i < n; i++)
