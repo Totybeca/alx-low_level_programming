766b342e989c954705585513fc344331bafdb3ab
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 15:31:48 -0700

    Added a 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,14 +2,13 @@
 #include "main.h"
 /**
 * more_numbers - function to print 0-14 10 times
-* 
-* Return: Numbers from 0-14 10 times
+* putchar only three times
+* Return: Numbers from 0-14 10 times followed by a new line
 */
 void more_numbers(void)
 {
 int i;
 int j;
-
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
