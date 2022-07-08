7c5596146497e11c18266c984cefd7a64072a52b
Totybeca <emeraldtoyin@gmail.com>
2022-07-07 18:03:35 -0700

    Added 100-prime_factors.c file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,10 @@
-#include <math.h>
 #include <stdio.h>
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
- */
+* main - prints prime factors of 612852475143
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int n;
-int max;
-int i;
-
-n = 612852475143;
-max = -1;
-{
-max = 2;
-n /= 2;
-}
-for (i = 3; 1 <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-max = i;
-n = n / i;
-}
-}
-if (n > 2)
-max = n;
-printf("%ld\n", max);
 return (0);
 }
