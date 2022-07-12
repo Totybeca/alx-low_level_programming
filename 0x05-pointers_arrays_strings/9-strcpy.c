3bfa5c536b1ae7f48d11fa538929e10f10f4140a
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 23:31:40 +0100

    Added a 8-prints_array.c  file to 0x0-pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy -  function that copies the string pointed to by src
+ * @dest: Where to copy the string to.
+ * @src: The source
+ * Return: A pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+int var = 0;
+while (src[var])
+{
+dest[var] = src[var];
+var++;
+}
+return (dest);
+}
