02143f4b7396a4eeff5de92bbbe885a3acb85ab3
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 23:51:04 +0100

    Added a 8-prints_array.c  file to 0x0-pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,12 @@
 char *_strcpy(char *dest, char *src)
 
 {
-int index = 0;
+int i = 0;
 
-while (src[index])
+while (src[i])
 {
-dest[index] = src[index];
-index++;
+dest[i] = src[i];
+i++;
 }
 
 return (dest);
