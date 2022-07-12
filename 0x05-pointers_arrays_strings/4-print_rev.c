f4b8f6c7928d176f854c3f3fc3f8d3d9b1549303
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 21:08:41 +0100

    Added a 4-print_rev.c  file to 0x0-pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,14 @@ void print_rev(char *s)
 int i;
 int n;
 
-n=0;
+n = 0;
 while (s[n] != '\0')
 n++;
 
-for(i = n - 1; i >= 0; i--)
+for (i = n - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
 _putchar('\n');
 }
+
