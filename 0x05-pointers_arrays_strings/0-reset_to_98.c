f1a30d56a88551624645c39d292fd17a847f0b31
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 14:56:54 +0100

    Added a  0-reset to 98.c file to 0x05 pointers array, and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer to an int and 
+ * updates the value it point to 98
+ * @n: input
+ * return: n
+ */
+void reset_to_98(int *n)
+{
+    int n;
+    n = 98;
+   *n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a read me file for today's project known as 0x05-pointers arrays and strings. This is for understanding of pointers, arrays and strings.
