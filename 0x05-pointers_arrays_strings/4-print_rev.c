5e12c202cb2001fcf782fb4e72e209489afb057a
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 21:06:50 +0100

    Added a 4-print_rev.c  file to 0x0-pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - a function that prints a string in reverse
+* @s: The string output
+*/
+void print_rev(char *s)
+{
+int i;
+int n;
+
+n=0;
+while (s[n] != '\0')
+n++;
+
+for(i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
