b167d93ed18b3fb78e68b0cd79e14a30319818be
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-12 17:28:30 +0100

    Added a 2-strlen.c file to 0x05-pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - This is a function that returns the length of a string
+ * @s: is a character input
+ * Return: The length of @s
+ */
+int _strlen(char *s)
+{
+size_t lenght =0;
+
+while (*s++)
+length++;
+return (length);
+}
