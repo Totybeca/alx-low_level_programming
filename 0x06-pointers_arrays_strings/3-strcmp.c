4fa285570812fcbea8a12df01e661644a033cfe8
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-14 21:56:12 +0100

    This is a 3-strcmp.c file added to 0x06-pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * _strcat - This is a function that concatenates two strings
  * @dest: Accepts the  src string
@@ -8,11 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int len = 0;
-int i;
+int len = 0, i;
 while (dest[len])
 len++;
-for(i = 0; src[i] != 0; i++)
+for (i = 0; src[i] != 0; i++)
 {
 dest[len] = src[i];
 len += 1;
