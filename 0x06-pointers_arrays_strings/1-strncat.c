92fdf20b3c20dd37e931cabaff198be97d2dce16
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-14 22:15:46 +0100

    This is a 3-strcmp.c file added to 0x06-pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - This is a function that concatenates two strings
+ * @dest: This is the first parameter
+ * @src: This is the second parameter
+ * @n: This is the third parameter
+ * Return: A string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	
+strncat(dest, src, n);
+return (dest);
+}
+
