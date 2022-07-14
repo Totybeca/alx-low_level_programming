900e72d8622c41d1190bdf774aa9a68a368a3230
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-14 23:19:35 +0100

    Added a 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - Function for copying
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+_strncpy(dest, src, n);
+return (dest);
+}
+
