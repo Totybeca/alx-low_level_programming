c6ab5aca7e76a6c71cb8edb8313b077366e123fa
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-18 21:33:33 +0100

    This is a 100.set_string.c file added to 0x07-pointers, arrays  and strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * set_string - Sets the value of a pointer to a char.
  * @s: The pointer that sets to char.
- * @to: The char .
+ * @to: The char.
  */
 void set_string(char **s, char *to)
 {
 	*s = to;
 }
+
