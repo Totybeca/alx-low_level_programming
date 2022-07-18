4c5d7d867bbb51d3c5b8ca6e166d66db31258745
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-07-18 17:23:28 +0100

    This is a 5-strpbrk.c file added to 0x07-pointers, arrays  and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to search.
+ * @needle: Substring to locate.
+ * Return: If the substring is located, point a pointer to the beginning
+ * if not, NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return (NULL);
+}
+
