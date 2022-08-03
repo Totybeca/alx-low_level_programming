80b08b7777123821cea51482a250034bafc62029
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-03 14:26:20 +0100

    Added a new file 0-print_name.c file to 0x0F-Functions_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#indlude "function_pointers.h"
+#include "function_pointers.h"
+
 /**
- * print_name -f points to func in main
- * @name: name
- * @f: pointer to print upp or non upp func
- * Return: void
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NUL || f == NULL)
-    return;
-    f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
 
