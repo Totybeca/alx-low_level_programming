568651398914470a0475b2ab56cacb05abe6b50c
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-03 14:40:02 +0100

    Added a 0-print_name.c file to 0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - Prints a name.
  * @name: The name to be printed.
@@ -9,9 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
-
