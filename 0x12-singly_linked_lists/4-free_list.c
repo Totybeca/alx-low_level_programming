2befae9c3b90bda020762617a6404fcffe0aee4b
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-11 16:52:44 +0100

    Added 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_list - a function that frees a list_t list.
- * @head: struct
- * Return: free
+ * free_list - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
  */
 void free_list(list_t *head)
 {
-if (head == NULL)
-return;
-
-if (head->next != NULL)
-free_list(head->next);
-free(head->str);
-free(head);
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
 }
