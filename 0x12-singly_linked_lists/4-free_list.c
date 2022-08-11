b8b55a8b0201620a1cdb20e8996ce602caaf45fd
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-11 16:41:48 +0100

    Added 100-first.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,11 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
+if (head == NULL)
+return;
 
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+if (head->next != NULL)
+free_list(head->next);
+free(head->str);
+free(head);
 }
