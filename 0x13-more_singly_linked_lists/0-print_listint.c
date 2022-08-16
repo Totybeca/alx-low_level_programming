aac4918c8982e913112b8bf6e2dc4c219c17613d
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-16 01:19:01 +0100

    Added two files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
-* print_listint - A function that prints all the elements in the list
-* @h: A pointer to listint structure
-* Return: the number of nodes.
-*/
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
+ *Return: The number of nodes in the list_t list.
+ */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
-
-while (h)
+int count = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-nodes++;
+count++;
 }
-return (nodes);
+return (count);
 }
