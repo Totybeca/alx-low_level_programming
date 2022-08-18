940dc6c7125a613af46111b6b472b1ab026c7cac
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-18 15:06:10 +0100

    Added a new file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,14 +3,12 @@
 /**
 * binary_to_uint -converts a binary number to an unsigned int.
 * @b: pointing to a string of 0 and 1 chars
-*
 * Return:converted number, or 0
 */
 unsigned int binary_to_uint(const char *b)
 {
 int i;
 unsigned int j;
-
 j = 0;
 if (!b)
 return (0);
