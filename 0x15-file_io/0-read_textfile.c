829d2f4555641194ecd7482fad58dbf0c5a46ae1
Totybeca <emeraldtoyin@gmail.comcd alx-pre_coursegit config --global user.email emeraldtoyin@gmail.com>
2022-08-22 17:11:46 +0100

    Added new files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,25 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file, rd, wr;
-char *buf;
-if (filename == NULL)
-return (0);
-file = open(filename, O_RDONLY);
-if (file == -1)
-return (0);
-buf = malloc(sizeof(char) * letters + 1);
-if (buf == NULL)
-return (0);
-rd = read(file, buf, letters);
-if (rd == -1)
-return (0);
-buf[letters] = '\0';
-wr = write(1, buf, rd);
-if (wr == -1)
-return (0);
-close(file);
-free(buf);
-return (wr);
-}
+ssize_t o, r, w;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
