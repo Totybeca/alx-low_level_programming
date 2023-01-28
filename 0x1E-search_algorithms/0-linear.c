f94b245a6e40f256e91c46af2dc660f357f818f9
Totybeca <emeraldtoyin@gmail.com>
2023-01-28 12:25:15 +0100

    linear-0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,13 +3,11 @@
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t x;
